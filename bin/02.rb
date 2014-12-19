f3e4d7b305ead149a5e53da2426cabd9d2f61d91
Jeff Hostetler <hostetjm@gmail.com>
2014-12-19 13:19:27 -0700

    fixed 02

--- bin/02.rb
+++ bin/02.rb
@@ -3,21 +3,19 @@ load '../data/crm.rb'
 
   def company_and_employees(data)
     result = []
-    data[:companies].each do |company|
-      data[:people].each do |person|
-        person[:employments].each do |employ|
-          entry = {}
-          entry[:company_id] = employ[:company_id]
-          entry[:company_name] = company[:name]
-          entry[:person_id] = person[:id]
-          entry[:person_first_name] = person[:first_name]
-          entry[:person_last_name] = person[:last_name]
-          entry[:title] = employ[:title]
-          result << entry
-        end
+    data[:people].each do |person|
+      person[:employments].each do |employ|
+        entry = {}
+        entry[:company_id] = employ[:company_id]
+        entry[:company_name] = data[:companies].select{|company| company[:id] == employ[:company_id]}[0][:name]
+        entry[:person_id] = person[:id]
+        entry[:person_first_name] = person[:first_name]
+        entry[:person_last_name] = person[:last_name]
+        entry[:title] = employ[:title]
+        result << entry
       end
     end
-    result
+  result
   end
 
 pp company_and_employees(CRM)
